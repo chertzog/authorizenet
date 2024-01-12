2df164fea58e91a956e744553e8e129d6be39f4c
Darren Shafae <darren@resumeblaze.com>
2024-01-11 21:45:57 -0800

    PHP8.3 does not allow Calling get_class() without arguments. It is deprecated and leaves a deprecation notice. To fix this deprecation notice, you must pass $this to the get_class() function.

--- lib/net/authorize/api/contract/v1/ProfileTransAuthOnlyType.php
+++ lib/net/authorize/api/contract/v1/ProfileTransAuthOnlyType.php
@@ -22,7 +22,7 @@ class ProfileTransAuthOnlyType extends ProfileTransOrderType implements \JsonSer
         });
         $mapper = \net\authorize\util\Mapper::Instance();
         foreach($values as $key => $value){
-            $classDetails = $mapper->getClass(get_class() , $key);
+            $classDetails = $mapper->getClass(get_class($this) , $key);
             if (isset($value)){
                 if ($classDetails->className === 'Date'){
                     $dateTime = $value->format('Y-m-d');
@@ -50,7 +50,7 @@ class ProfileTransAuthOnlyType extends ProfileTransOrderType implements \JsonSer
         if(is_array($data) || is_object($data)) {
 			$mapper = \net\authorize\util\Mapper::Instance();
 			foreach($data AS $key => $value) {
-				$classDetails = $mapper->getClass(get_class() , $key);
+				$classDetails = $mapper->getClass(get_class($this) , $key);
 	 
 				if($classDetails !== NULL ) {
 					if ($classDetails->isArray) {
--- lib/net/authorize/api/contract/v1/TransactionRequestType.php
+++ lib/net/authorize/api/contract/v1/TransactionRequestType.php
@@ -1127,7 +1127,7 @@ class TransactionRequestType implements \JsonSerializable
         });
         $mapper = \net\authorize\util\Mapper::Instance();
         foreach($values as $key => $value){
-            $classDetails = $mapper->getClass(get_class() , $key);
+            $classDetails = $mapper->getClass(get_class($this) , $key);
             if (isset($value)){
                 if ($classDetails->className === 'Date'){
                     $dateTime = $value->format('Y-m-d');
@@ -1155,7 +1155,7 @@ class TransactionRequestType implements \JsonSerializable
         if(is_array($data) || is_object($data)) {
 			$mapper = \net\authorize\util\Mapper::Instance();
 			foreach($data AS $key => $value) {
-				$classDetails = $mapper->getClass(get_class() , $key);
+				$classDetails = $mapper->getClass(get_class($this) , $key);
 	 
 				if($classDetails !== NULL ) {
 					if ($classDetails->isArray) {
